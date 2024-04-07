f673fa93fe6931c50a86970ab1f5fddfcb549f8e
niraj1234 <niraj.webtech@gmail.com>
2024-04-08 00:18:57 +0530

    news end point added

--- src/main/java/com/niraj/controller/WelcomeController.java
+++ src/main/java/com/niraj/controller/WelcomeController.java
@@ -16,4 +16,11 @@ public class WelcomeController {
 		System.out.println("Before any PUSH to gitHub");
 		return "Welcome to Git-Colt project ";
      }
+	
+	@RequestMapping("/news")
+	public String news() {
+		System.out.println("Adding news REST end point");
+		return "news rest end point added";
+     }
+
 }
