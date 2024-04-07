ad250407b5d8a92abc01f7873718257231ab5fa0
niraj1234 <niraj.webtech@gmail.com>
2024-04-08 01:17:38 +0530

    dashboard function added

--- src/main/java/com/niraj/controller/WelcomeController.java
+++ src/main/java/com/niraj/controller/WelcomeController.java
@@ -30,8 +30,10 @@ public class WelcomeController {
 		System.out.println("Adding product REST");
 		return "product rest point added";
      }
-
-
 	
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}	
 	
 }
