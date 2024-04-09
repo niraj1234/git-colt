e737427b08b9ea0b44a6de4e2f475e0da204fa44
niraj1234 <niraj.webtech@gmail.com>
2024-04-09 20:01:05 +0530

    Just check 3

--- src/main/java/com/niraj/controller/WelcomeController.java
+++ src/main/java/com/niraj/controller/WelcomeController.java
@@ -19,7 +19,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/news")
 	public String news() {
-		System.out.println("Changing directly from GitHub change 3");
+		System.out.println("Removing last hour changes");
 		return "news rest end point added";
      }
 
