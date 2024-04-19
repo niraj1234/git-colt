986899aa906d454aef0abc3fb5779056bcecca0d
niraj1234 <niraj.webtech@gmail.com>
2024-04-19 12:45:19 +0530

    Just checking token

--- src/main/java/com/niraj/controller/WelcomeController.java
+++ src/main/java/com/niraj/controller/WelcomeController.java
@@ -19,7 +19,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/news")
 	public String news() {
-		System.out.println("Change on 11th April Actuator dependency added");
+		System.out.println("Change on 19th April Just Checking Token");
 		return "news rest end point added";
      }
 
